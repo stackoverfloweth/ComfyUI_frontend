842aa5381dedfb12e7f68cbedcdc1ff5bb0ee470
GitHub Action <action@github.com>
2026-02-14 00:52:29 +0000

    [automated] Apply ESLint and Oxfmt fixes

--- src/renderer/extensions/vueNodes/components/LGraphNode.vue
+++ src/renderer/extensions/vueNodes/components/LGraphNode.vue
@@ -217,7 +217,9 @@
         </template>
       </Button>
     </div>
-    <template v-if="!isCollapsed && !isRerouteNode && nodeData.resizable !== false">
+    <template
+      v-if="!isCollapsed && !isRerouteNode && nodeData.resizable !== false"
+    >
       <div
         v-for="handle in RESIZE_HANDLES"
         :key="handle.corner"
